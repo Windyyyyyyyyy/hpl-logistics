092d11018c749b6fb19bb994c4951a17aafc4803
ChiCuong002 <buichicuong6110@gmail.com>
2024-12-31 11:59:42 +0700

    remove sensitive data

--- src/pages/Contact/firebase.ts
+++ src/pages/Contact/firebase.ts
@@ -7,14 +7,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBTylZTD0vivT81GNy7huZmv9BOTY4Uq_Q",
-  authDomain: "hpl-logistic.firebaseapp.com",
-  databaseURL: "https://hpl-logistic-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  projectId: "hpl-logistic",
-  storageBucket: "hpl-logistic.firebasestorage.app",
-  messagingSenderId: "309340390417",
-  appId: "1:309340390417:web:367e9df244408ba2001db0",
-  measurementId: "G-XQFSBHL1QT"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase only if it hasn't been initialized
